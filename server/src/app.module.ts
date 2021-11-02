b349f4f90643f131522e75f17e3ad68656170f59
Jordan Renaud <jtrenaud1s@semo.edu>
2021-11-02 16:52:02 -0500

    testing

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.gql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
